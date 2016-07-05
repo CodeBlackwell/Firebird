129441d1bd11ad97d7bb334ac571c04938c16113
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-07-05 14:01:20 -0700

    Succesfully calling GET 5 times. @TODO: Handle error when no more results are found

--- Server.js
+++ Server.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var twit = require('twitter');
+var Twit = require('twitter');
 var config = require('./config.js');
 var helper = require('./helpers.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var twitter = new twit(config);
+var twitter = new Twit(config);
 
 
 var error = function (err, response, body) {
-    console.log('ERROR [%s]', JSON.stringify(err));
+  console.log('ERROR [%s]', JSON.stringify(err));
 };
 var success = function (data) {
-    console.log('Data [%s]', data);
+  console.log('Data [%s]', data);
 };
 
 
@@ -32,43 +32,50 @@ var server = app.listen(3000);
 *Using twitter api to retrieve tweets
 */
 
-var URL = encodeURIComponent("#realestate");
-var tweetStorage = [{id: 0}];
-var theLastTweet = tweetStorage[tweetStorage.length - 1].id;
+var URL = encodeURIComponent('#wtf');
+var tweetStorage = [];
 
-twitter.get("https://api.twitter.com/1.1/search/tweets.json?q=" + URL + "&since_id=" + theLastTweet + "&count=100&src=typd", function(err1, res1) {
+
+twitter.get('https://api.twitter.com/1.1/search/tweets.json?q=' + URL + '&since_id=0&count=100&src=typd', function(err1, res1) {
+  if (err1) { 
+    console.log(err1);
+  } else {}
   tweetStorage.push(res1.statuses);
-  console.log("the first query is pushed");
+  console.log('the first query is pushed', tweetStorage.length);
   console.log(Object.keys(res1));
-  twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res1.search_metadata.next_results + "&src=typd", function(err2, res2){
+  twitter.get('https://api.twitter.com/1.1/search/tweets.json' + res1.search_metadata.next_results + '&since_id=0&src=typd', function(err2, res2) {
+    if (err2) {
+      console.log(err2);
+
+    }
     tweetStorage.push(res2.statuses);
-    console.log("the second query is pushed");
+    console.log('the second query is pushed', tweetStorage.length);
     console.log(Object.keys(res2));
-    twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res2.search_metadata.next_results + "&src=typd", function(err3, res3){
+    twitter.get('https://api.twitter.com/1.1/search/tweets.json' + res2.search_metadata.next_results + '&since_id=0&src=typd', function(err3, res3) {
+      if (err3) {
+        console.log(err3);
+      }
       tweetStorage.push(res3.statuses);
-      console.log("the third query is pushed");
+      console.log('the third query is pushed', tweetStorage.length);
       console.log(Object.keys(res3));
-      twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res3.search_metadata.next_results + "&src=typd", function(err4, res4){
+      twitter.get('https://api.twitter.com/1.1/search/tweets.json' + res3.search_metadata.next_results + '&since_id=0&src=typd', function(err4, res4) {
+        if (err4) { 
+          console.log(err4);
+        }
         tweetStorage.push(res4.statuses);
-        console.log("the fourth query is pushed");
+        console.log('the fourth query is pushed', tweetStorage.length);
         console.log(Object.keys(res4));
-        twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res4.search_metadata.next_results + "&src=typd", function(err5,res5) {
+        twitter.get('https://api.twitter.com/1.1/search/tweets.json' + res4.search_metadata.next_results + '&since_id=0&src=typd', function(err5, res5) {
+          if (err5) { 
+            console.log(err5);
+          }
           tweetStorage.push(res5.statuses);
-          console.log("the fifth query is pushed");
+          console.log('the fifth query is pushed', tweetStorage.length);
           console.log(Object.keys(res5));
-          console.log("Here is how many status you can search ^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%" + tweetStorage.length);
+          console.log('Here is how many status you can search ^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%' + tweetStorage.length);
         });
       });
     });
   });
 });
-
-
-
-
-
-
-
-
-
 
