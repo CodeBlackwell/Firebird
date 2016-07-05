8856188c0f3e4c38c3d2b74d16b6b1432cded041
LeChristopher Blackwell <codeblackwell@gmail.com>
2016-07-05 11:35:49 -0700

    5 100-count searches. Error @ 4th Query

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var twit = require('twitter');
 var config = require('./config.js');
+var helper = require('./helpers.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -35,8 +36,39 @@ var URL = encodeURIComponent("#realestate");
 var tweetStorage = [{id: 0}];
 var theLastTweet = tweetStorage[tweetStorage.length - 1].id;
 
-twitter.get("https://api.twitter.com/1.1/search/tweets.json?q=" + URL + "&since_id=" + theLastTweet + "&count=10&src=typd", function(err, res) {
-  console.log(res, res.statuses.length);
-  
-  
+twitter.get("https://api.twitter.com/1.1/search/tweets.json?q=" + URL + "&since_id=" + theLastTweet + "&count=100&src=typd", function(err1, res1) {
+  tweetStorage.push(res1.statuses);
+  console.log("the first query is pushed");
+  console.log(Object.keys(res1));
+  twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res1.search_metadata.next_results + "&src=typd", function(err2, res2){
+    tweetStorage.push(res2.statuses);
+    console.log("the second query is pushed");
+    console.log(Object.keys(res2));
+    twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res2.search_metadata.next_results + "&src=typd", function(err3, res3){
+      tweetStorage.push(res3.statuses);
+      console.log("the third query is pushed");
+      console.log(Object.keys(res3));
+      twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res3.search_metadata.next_results + "&src=typd", function(err4, res4){
+        tweetStorage.push(res4.statuses);
+        console.log("the fourth query is pushed");
+        console.log(Object.keys(res4));
+        twitter.get("https://api.twitter.com/1.1/search/tweets.json" + res4.search_metadata.next_results + "&src=typd", function(err5,res5) {
+          tweetStorage.push(res5.statuses);
+          console.log("the fifth query is pushed");
+          console.log(Object.keys(res5));
+          console.log("Here is how many status you can search ^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%" + tweetStorage.length);
+        });
+      });
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
